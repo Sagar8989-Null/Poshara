a46e853cb0a5efa5b5df3bfd5c8881c29f7d7e64
Sagar Jamadar <sagarjamadar8989@gmail.com>
2025-10-29 02:31:31 +0530

    changed the connection type to Pool and conenction limit =10

--- Backend/server.js
+++ Backend/server.js
@@ -12,11 +12,13 @@ app.use(cors()); // allow React frontend
 app.use(express.json());
 
 // MySQL connection
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
 // Connect to DB
