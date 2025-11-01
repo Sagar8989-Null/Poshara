793eb41df48c39e7633715ec532a7164242944c8
Sagar Jamadar <sagarjamadar8989@gmail.com>
2025-11-01 16:26:22 +0530

    Strict Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           <Route 
             path="/dashboard/restaurant" 
             element={
-              <ProtectedRoute>
+              <ProtectedRoute requiredRole="restaurant">
                 <RestoDash />
               </ProtectedRoute>
             } 
@@ -31,7 +31,7 @@ function App() {
           <Route 
             path="/dashboard/ngo" 
             element={
-              <ProtectedRoute>
+              <ProtectedRoute requiredRole="ngo">
                 <NgoDash />
               </ProtectedRoute>
             } 
@@ -39,7 +39,7 @@ function App() {
           <Route 
             path="/dashboard/volunteer" 
             element={
-              <ProtectedRoute>
+              <ProtectedRoute requiredRole="volunteer">
                 <VolDash />
               </ProtectedRoute>
             } 
