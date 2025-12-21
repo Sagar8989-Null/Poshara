de2bb0ab7a69f29d399450cb2e420e621d964fc5
Sagar Jamadar <sagarjamadar8989@gmail.com>
2025-12-21 23:04:55 +0530

    server.js updated

--- Backend/database/server.js
+++ Backend/database/server.js
@@ -17,5 +17,9 @@ app.use(express.json());
 app.use("/api/donations", donationRoutes);
 app.use("/api/volunteer", volunteerRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.get("/api/health", (req, res) => {
+  res.json({ status: "OK", time: new Date() });
+});
+
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
