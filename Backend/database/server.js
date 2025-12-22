467786b0f6447b67a3e3f51517730e0a1a0ea26e
Sagar Jamadar <sagarjamadar8989@gmail.com>
2025-12-23 01:02:10 +0530

    updated the CORS origin at backend

--- Backend/database/server.js
+++ Backend/database/server.js
@@ -13,12 +13,13 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+const Frontend_URL= process.env.Frontend_URL;
 
 
 
 
 app.use(cors({
-  origin: "https://poshara.netlify.app/", // replace with frontend domain in prod
+  origin: Frontend_URL, // replace with frontend domain in prod
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(express.json());
@@ -28,7 +29,7 @@ app.use(express.json());
 
 const io = new Server(server, {
   cors: {
-    origin: "https://poshara.netlify.app/", // replace with frontend URL
+    origin: Frontend_URL, // replace with frontend URL
     methods: ["GET", "POST"],
   },
 });
